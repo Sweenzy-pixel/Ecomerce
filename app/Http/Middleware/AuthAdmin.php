9c1307bd0f8e8f0ccff85a00f51e2456983ac142
Sweenzy-pixel~ <minozagab07@gmail.com~>
2025-05-29 18:58:05 +0800

    first commit

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            if (Auth::user()->utype === 'ADM') {
+                return $next($request);
+            } else {
+                Session::flush();
+                return redirect('login');
+            }
+        } else {
+            return redirect('login');
+        }
+    }
+}
